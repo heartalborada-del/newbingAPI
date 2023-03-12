aba3f4257fdcfbf1691d483f0a3fa6e106176a48
heartalborada-del <heartalborada@outlook.com>
2023-03-12 13:36:47 +0800

    改了一点点而已

--- src/main/java/me/heartalborada/newBingAPI/exceptions/ConversationException.java
+++
@@ -1,8 +0,0 @@
-package me.heartalborada.newBingAPI.exceptions;
-
-public class ConversationException extends Exception {
-    //private static final long serialVersionUID = -7041169491254546905L;
-    public ConversationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/me/heartalborada/newBingAPI/types/chat/participant.java
+++
@@ -1,9 +0,0 @@
-package me.heartalborada.newBingAPI.types.chat;
-
-public class participant {
-    private final String id;
-
-    public participant(String id) {
-        this.id = id;
-    }
-}
